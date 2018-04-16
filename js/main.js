ac68b5b7da7e68af30c92cd89281731f987de1da
Prashant Shrestha <pms@engineer.com>
2018-04-16 16:53:30 -0400

    Left the script in raw format!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,69 @@
-$(document).ready(function(){var f=function(a,b){if(b==true){return a.replace('+','-').replace('/','_')}else{return a.replace('-','+').replace('_','/')}};var g=function(b){var a=document.createElement('a');a.href=b;return(a.host&&a.host!=window.location.host)};var h=window.location.href;var i=window.location.origin+window.location.pathname;$("a.encodeURL").on('click',function(e){e.preventDefault();var a=$("#toEncode");if(a.val().length>0&&g(a.val())){var b=f(btoa(a.val()),true);a.val(h+"#!"+b)}else{alert("Please make sure to provide a proper yet valid URL!")}return false});var j=function(){var a=window.location.href;if(a.indexOf('#!')>0){var b=a.split('#!').pop();if(b.length>0){$("form.encodeForm").toggle('display');var c=f(b,false);var d=atob(c);$("a.decodedURL").text(d);$("a.decodedURL").attr('href',d)}else{}}};var k=function(){console.log("Designed and Developed by Prashant Shrestha © 2017.");console.log("Stop by and say 'Hi!' @ https://prashant.me/");console.log("Please use it at your own risk, this is simply a prototype of an idea and I (Prashant Shrestha) cannot be held liable for any deeds/misdeeds done via. this project.");console.log("Special thanks to ClipboardJS, SkeletonCSS, Github Pages & IcoMoon!")};j();$("a.baseURL").attr('href',i);new Clipboard('.copytoClipboard');k()});$(document).ready(function(){var f=function(a,b){if(b==true){return a.replace('+','-').replace('/','_')}else{return a.replace('-','+').replace('_','/')}};var g=function(b){var a=document.createElement('a');a.href=b;return(a.host&&a.host!=window.location.host)};var h=window.location.href;var i=window.location.origin+window.location.pathname;$("a.encodeURL").on('click',function(e){e.preventDefault();var a=$("#toEncode");if(a.val().length>0&&g(a.val())){var b=f(btoa(a.val()),true);a.val(h+"#!"+b)}else{alert("Please make sure to provide a proper yet valid URL!")}return false});var j=function(){var a=window.location.href;if(a.indexOf('#!')>0){var b=a.split('#!').pop();if(b.length>0){$("form.encodeForm").toggle('display');var c=f(b,false);var d=atob(c);$("a.decodedURL").text(d);$("a.decodedURL").attr('href',d)}else{}}};var k=function(){console.log("Designed and Developed by Prashant Shrestha © 2017.");console.log("Stop by and say 'Hi!' @ https://prashant.me/");console.log("Please use it at your own risk, this is simply a prototype of an idea and I (Prashant Shrestha) cannot be held liable for any deeds/misdeeds done via. this project.");console.log("Special thanks to ClipboardJS, SkeletonCSS, Github Pages & IcoMoon!")};j();$("a.baseURL").attr('href',i);new Clipboard('.copytoClipboard');k()});
+$(document).ready(function()
+	{
+	var cleanURLParameter=function(content,toEncode)
+	{
+		if(toEncode==true)
+		{
+			return content.replace('+','-').replace('/','_')
+		}
+		else
+		{
+			return content.replace('-','+').replace('_','/')
+		}
+	};
+	var isValidURL=function(str)
+	{
+		var a=document.createElement('a');
+		a.href=str;
+		return(a.host&&a.host!=window.location.host)
+	};
+	var baseurl=window.location.href;
+	var projectURL=window.location.origin+window.location.pathname;
+	$("a.encodeURL").on('click',function(e)
+		{
+		e.preventDefault();
+		var _0=$("#toEncode");
+		if(_0.val().length>0&&isValidURL(_0.val()))
+		{
+			var cleanURLParam=cleanURLParameter(btoa(_0.val()),true);
+			_0.val(baseurl+"#!"+cleanURLParam)
+		}
+		else
+		{
+			alert("Please make sure to provide a proper yet valid URL!")
+		}
+		return false
+		}
+	);
+	var getURLParameters=function()
+	{
+		var url=window.location.href;
+		if(url.indexOf('#!')>0)
+		{
+			var arguments=url.split('#!').pop();
+			if(arguments.length>0)
+			{
+				$("form.encodeForm").toggle('display');
+				var cleanEncodedURLParam=cleanURLParameter(arguments,false);
+				var cleanDecodedURL=atob(cleanEncodedURLParam);
+				$("a.decodedURL").text(cleanDecodedURL);
+				$("a.decodedURL").attr('href',cleanDecodedURL)
+			}
+			else
+			{
+			}
+		}
+	};
+	var printCredits=function()
+	{
+		console.log("Designed and Developed by Prashant Shrestha © 2017.");
+		console.log("Stop by and say 'Hi!' @ https://prashant.me/");
+		console.log("Please use it at your own risk, this is simply a prototype of an idea and I (Prashant Shrestha) cannot be held liable for any deeds/misdeeds done via. this project.");
+		console.log("Special thanks to ClipboardJS, SkeletonCSS, Github Pages & IcoMoon!");
+	};
+	getURLParameters();
+	$("a.baseURL").attr('href',projectURL);
+	new Clipboard('.copytoClipboard');
+	printCredits()
+	}
+);
